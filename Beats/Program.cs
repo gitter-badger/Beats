18e3bd4aabe46ecca07b7864012b356b9a8bf970
default0 <Kevin.Labschek@gmx.de>
2015-11-13 17:28:15 +0100

    Create Beats project
    Set up the bare bones for the beats project. For now this is just
    a black OpenGL-Window.

---
+++ Beats/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beats
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			using (Window window = new Window())
+				window.Run();
+		}
+	}
+}
---
+++ Beats/Window.cs
@@ -0,0 +1,29 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+
+namespace Beats
+{
+	public class Window : GameWindow
+	{
+		public Window()
+			: base(1024, 768)
+		{
+
+		}
+
+		protected override void OnRenderFrame(FrameEventArgs e)
+		{
+			base.OnRenderFrame(e);
+
+			GL.ClearColor(Color.Black);
+			GL.Clear(ClearBufferMask.ColorBufferBit);
+
+			SwapBuffers();
+		}
+		protected override void OnUpdateFrame(FrameEventArgs e)
+		{
+			base.OnUpdateFrame(e);
+		}
+	}
+}
