e4ae3ae6b2edd1c30080154e83d94cf40258d35e
PMDRose <shiro@ppy.sh>
2015-11-15 01:07:40 +0100

    Add basic Scene code
    Adds the Scene class with the constructor and reset method coded and an empty draw method to be completed.

---
+++ Beats/Scenes/Scene.cs
@@ -0,0 +1,44 @@
+﻿using Beats.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beats.Scenes
+{
+    /// <summary>
+	/// Represents a scene. A scene has a list of sprites it will draw, can be reset and transition to other scenes.
+	/// </summary>
+    class Scene
+    {
+        /// <summary>
+        /// The list of sprites the scene uses.
+        /// </summary>
+        private Sprite[] spriteList;
+
+        /// <summary>
+        /// Constructor for the Scene class. Creates an empty list of Sprites with the specified size.
+        /// </summary>
+        /// <param name="n">The size of the sprite list.</param>
+        public Scene(int n)
+        {
+            spriteList = new Sprite[n];
+        }
+
+        /// <summary>
+        /// Draws the scene.
+        /// </summary>
+        public void draw()
+        {
+        }
+
+        /// <summary>
+        /// Resets the scene back to a new one.
+        /// </summary>
+        public void reset()
+        {
+            spriteList = null;
+        }
+    }
+}
