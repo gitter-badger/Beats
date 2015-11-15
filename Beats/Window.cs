d530bad7eee6690e01bf52f0bd0ca705d49b55fb
default0 <Kevin.Labschek@gmx.de>
2015-11-15 13:02:47 +0100

    Add scene handling to window
    The window now controls what scenes are active and instruments
    transitioning logic of the scenes. It also draws all active scenes.

--- Beats/Window.cs
+++ Beats/Window.cs
@@ -4,6 +4,8 @@ using OpenTK;
 using OpenTK.Graphics.OpenGL;
 using System.Drawing;
 using System;
+using System.Collections.Generic;
+using Beats.Scenes;
 
 namespace Beats
 {
@@ -12,6 +14,8 @@ namespace Beats
 	/// </summary>
 	public class Window : GameWindow
 	{
+		private List<Scene> activeScenes;
+
 		/// <summary>
 		/// Constructs the main window.
 		/// </summary>
@@ -24,6 +28,8 @@ namespace Beats
 			GL.EnableClientState(ArrayCap.VertexArray);
 			GL.Enable(EnableCap.Blend);
 			GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+
+			activeScenes = new List<Scene>();
 		}
 
 		private void fixViewPort()
@@ -40,6 +46,37 @@ namespace Beats
 			GL.Translate(0f, -Height, 0f);
 		}
 
+		/// <summary>
+		/// Transitions from the given old scene to the given new scene.
+		/// </summary>
+		/// <param name="oldScene">The old scene that should be replaced with the new scene.</param>
+		/// <param name="newScene">The new scene that should replace the given old scene.</param>
+		public void Transition(Scene oldScene, Scene newScene)
+		{
+			int oldSceneIndex = activeScenes.IndexOf(oldScene);
+			if (oldSceneIndex == -1)
+				throw new Exception($"Attempted to transition from non-active scene {oldScene.Name} to {newScene.Name}");
+
+			int newSceneIndex = activeScenes.IndexOf(newScene);
+			if (newSceneIndex != -1)
+				throw new Exception($"Attempted to transition from {oldScene.Name} to already-active scene {newScene.Name}");
+
+			if (!oldScene.CanTransitionTo(newScene))
+				throw new Exception($"Attempted invalid transition from {oldScene.Name} to {newScene.Name}");
+
+			EventHandler<EventArgs> onTransitionOutFinished = null; // this is necessary due to definite-assignment rules.
+			onTransitionOutFinished = (sender, args) =>
+			{
+				activeScenes.Remove(oldScene);
+				oldScene.TransitionFinished -= onTransitionOutFinished;
+			};
+			oldScene.TransitionFinished += onTransitionOutFinished;
+			oldScene.TransitionOut();
+
+			activeScenes.Add(newScene);
+			newScene.TransitionIn();
+		}
+
 		protected override void OnResize(EventArgs e)
 		{
 			base.OnResize(e);
@@ -50,6 +87,8 @@ namespace Beats
 			base.OnRenderFrame(e);
 
 			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+			foreach (Scene scene in activeScenes)
+				scene.Draw();
 
 			SwapBuffers();
 		}
