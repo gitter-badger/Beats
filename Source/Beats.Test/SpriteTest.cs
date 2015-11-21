bd7a26b5753c4f6e407364725ca1637a58573654
default0 <Kevin.Labschek@gmx.de>
2015-11-21 09:56:34 +0100

    Add Unit-Test project
    Whenever there's functionality that should be unit-tested tests
    for it can be written in this project.

---
+++ Source/Beats.Test/SpriteTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Beats.Sprites;
+using System.Linq;
+
+namespace Beats.Test
+{
+	[TestClass]
+	public class SpriteTest
+	{
+		[TestMethod]
+		public void TestAllChildren()
+		{
+			TestSprite sprite = new TestSprite();
+			TestSprite sprite2 = new TestSprite();
+			TestSprite sprite3 = new TestSprite();
+			sprite.AddChild(sprite2);
+			sprite2.AddChild(sprite3);
+
+			Assert.IsTrue(sprite.AllChildren.Count() == 2);
+		}
+	}
+}
---
+++ Source/Beats.Test/TestSprite.cs
@@ -0,0 +1,22 @@
+﻿using Beats.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beats.Test
+{
+	public class TestSprite : Sprite
+	{
+		public void AddChild(Sprite child)
+		{
+			addChild(child);
+		}
+
+		public void RemoveChild(Sprite child)
+		{
+			removeChild(child);
+		}
+	}
+}
