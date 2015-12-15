95687284246821ef0f5efc8f55be4a99914538e5
default0 <Kevin.Labschek@gmx.de>
2015-12-16 00:54:39 +0100

    Add sound-data information to maps
    Contains information on what sound files to play when.

---
+++ Source/Beats/Audio/SoundData.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beats.Audio
+{
+	/// <summary>
+	/// Represents sound data that has information on what to play when.
+	/// </summary>
+	public class SoundData
+	{
+		/// <summary>
+		/// The absolute time offset in ms from the start of the map at which this sound will be played.
+		/// </summary>
+		[JsonProperty("offset")]
+		public int Offset { get; private set; }
+
+		/// <summary>
+		/// The path to the sound file that should be played.
+		/// </summary>
+		[JsonProperty("soundFile")]
+		public string SoundFile { get; private set; }
+	}
+}
--- Source/Beats/Maps/Map.cs
+++ Source/Beats/Maps/Map.cs
@@ -1,10 +1,37 @@
-﻿using Newtonsoft.Json;
+﻿using Beats.Audio;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Beats.Maps
 {
-    public class Map
-    {
-        [JsonRequired, JsonProperty("name")]
-        public string Name { get; private set; }
-    }
+	/// <summary>
+	/// Represents a map. A map is the main element of gameplay and contains
+	/// hitobjects, sounds and other data required for a play session.
+	/// </summary>
+	public class Map
+	{
+		/// <summary>
+		/// The name of the map.
+		/// </summary>
+		[JsonRequired, JsonProperty("name")]
+		public string Name { get; private set; }
+
+		[JsonRequired]
+		private List<SoundData> sounds { get; set; }
+
+		/// <summary>
+		/// The sounds that are played on this map, including the music as well as other sound effects.
+		/// </summary>
+		public ReadOnlyCollection<SoundData> Sounds { get; private set; }
+
+		/// <summary>
+		/// Constructs a new, empty map.
+		/// </summary>
+		public Map()
+		{
+			sounds = new List<SoundData>();
+			Sounds = new ReadOnlyCollection<SoundData>(sounds);
+		}
+	}
 }
